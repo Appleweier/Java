b0407b34e042ba15b09c460e804fa5754de97d02
WuHan <905284451@qq.com>
2019-03-08 12:38:31 +0800

    Test

---
+++ src/Course.java
@@ -0,0 +1,21 @@
+public class Course {
+    double MATH;
+    double JAVA;
+    Course(){}
+
+    public void setJAVA(double j) {
+        this.JAVA = j;
+    }
+
+    public void setMATH(double m) {
+        this.MATH = m;
+    }
+
+    public double getJAVA() {
+        return JAVA;
+    }
+
+    public double getMATH() {
+        return MATH;
+    }
+}
---
+++ src/Score.java
@@ -0,0 +1,30 @@
+public class Score {
+    String num;
+    String name;
+    double score;
+    double math;
+    double java;
+
+    public Score(){
+
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public void setJava(double java) {
+        this.java = java;
+    }
+
+    public void setMath(double math) {
+        this.math = math;
+    }
+}
